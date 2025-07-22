af7422ed5477598a03d293256e8da818e9c3f862
George Merrison <georgevicitunes@hotmail.co.uk>
2025-07-22 11:50:32 +0100

    pokemone group project improved

--- group_project_poke.py
+++ group_project_poke.py
@@ -29,15 +29,22 @@ cpu_name = cpu_data["name"]
 cpu_height = int(cpu_data['height']) / 10
 cpu_weight = int(cpu_data['weight']) / 10
 
-def get_attack_stat(data):
-    for stat in data['stats']:
-        if stat['stat']['name'] == 'attack':
+def get_stat(data, stat_name):
+    """
+    Helper function to get a specific stat from pokemon data.
+    """
+    for stat in data["stats"]:
+        if stat['stat']['name'] == stat_name:
             return stat['base_stat']
     return 0
 
-
-player_attack = get_attack_stat(pokemon_data)
-cpu_attack = get_attack_stat(cpu_data)
+# Get stats for player and CPU
+player_attack = get_stat(pokemon_data, "attack")
+cpu_attack = get_stat(cpu_data, "attack")
+player_defense = get_stat(pokemon_data, "defense")
+cpu_defense = get_stat(cpu_data, "defense")
+player_speed = get_stat(pokemon_data, "speed")
+cpu_speed = get_stat(cpu_data, "speed")
 
 # to get ability
 abilities = pokemon_data['abilities'][0]
@@ -55,11 +62,15 @@ print('name: {}'.format(pokemon_data['name']))
 print('weight: {}'.format(weight_formatted) + "(kgs)")
 print('height: {}'.format(height_formatted) + "(m)")
 print(f"Your Attack: {player_attack}")
+print(f"Your Defense: {player_defense}")
+print(f"Your Speed: {player_speed}")
 print('ability: {}'.format(ability['name']))
 
 print("\n   CPU Pokemon    ")
 print(f"name: {cpu_name}")
-print(f"attack {cpu_attack}")
+print(f"attack: {cpu_attack}")
+print(f"defense: {cpu_defense}")
+print(f"speed: {cpu_speed}")
 print(f"weight: {cpu_weight}(kgs)")
 print(f"height: {cpu_height}(m)")
 
@@ -67,12 +78,20 @@ print(f"height: {cpu_height}(m)")
 print("\n    battle results   ")
 print(f"Your Pokémon: {pokemon_data['name'].title()}")
 print(f"Your Attack: {player_attack}")
+print(f"Your Defense: {player_defense}")
+print(f"Your Speed: {player_speed}")
 print(f"CPU Pokémon: {cpu_data['name'].title()}")
 print(f"CPU Attack: {cpu_attack}")
+print(f"CPU Defense: {cpu_defense}")
+print(f"CPU Speed: {cpu_speed}")
 
-if player_attack > cpu_attack:
-    print("You win!")
-elif cpu_attack > player_attack:
-    print("CPU wins!")
+# Simple battle logic based on combined stats
+player_total_power = player_attack + player_defense + player_speed
+cpu_total_power = cpu_attack + cpu_defense + cpu_speed
+
+if player_total_power > cpu_total_power:
+    print(f"{pokemon_data['name'].title()} wins!")
+elif cpu_total_power > player_total_power:
+    print(f"{cpu_data['name'].title()} wins!")
 else:
     print("It's a draw!")
